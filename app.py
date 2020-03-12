8085f4d55074c2913fcdade915392b7a9656f7c2
Iresh4228 <iresh.walisundara24@gmail.com>
2020-03-12 15:35:30 +0530

    Finalized report

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, jsonify
 from pymongo import MongoClient
 from bson import json_util
+from setuptools import setup
+import requests
+
 
 app = Flask(__name__)
 
@@ -12,11 +15,25 @@ collection = db.to_dos
 def index():
     return render_template('index.html')
 
+@app.route('/search')
+def search():
+
+    seasons = request.args.get('a', 0, type=str)
+
+    response = requests.get(f"https://www.balldontlie.io/api/v1/stats?seasons={seasons}")
+
+    response = response.json()["seasons"][0]
+    print(response)
+    return jsonify(result=response)
+
+
 
 @app.route('/view', methods=['GET'])
 def get_todos():
     to_dos = list(collection.find())
     return json_util.dumps(to_dos)
+
+
 
 
 @app.route('/add', methods=['POST'])
@@ -37,4 +54,3 @@ def add_todo():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
-
